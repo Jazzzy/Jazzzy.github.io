f379798fa4ba72e64351336931916856cfc4ebbd
Rubén Osorio López <Rubn Osorio Lpez>
2017-04-18 20:40:21 +0200

    Starting to work on this

---
+++ js/mainWeb.js
@@ -0,0 +1,13 @@
+$( document ).ready( onReadyDocument );
+
+
+function onReadyDocument() {
+  setUpParallax();
+}
+
+
+function setUpParallax() {
+  $( ".parallax" ).parallax();
+
+  //Read here width and heigth of the window and update the heigth and width of the parallax-container and parallax-image.
+}
