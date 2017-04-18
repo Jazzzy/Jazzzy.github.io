bbc5a43581ac14bac9669dfaf0512d7c63857c5e
Rubén Osorio López <Rubn Osorio Lpez>
2017-04-19 01:42:05 +0200

    Starting to work on this

--- js/mainWeb.js
+++ js/mainWeb.js
@@ -1,13 +1,28 @@
 $( document ).ready( onReadyDocument );
-
+$( window ).resize( onResizeWindow );
 
 function onReadyDocument() {
   setUpParallax();
+  setUpTabs();
 }
 
+function onResizeWindow() {
+  setUpParallax();
+}
 
+function setUpTabs() {
+  $( 'ul.tabs' ).tabs();
+}
+
+/*
+  The images with a parallax effect should have a ratio bigger than 3/2 more or
+  less since the parallax window is 3/1
+*/
 function setUpParallax() {
-  $( ".parallax" ).parallax();
+  $( ".parallax" ).parallax( 115 ); //We can change this value to change the parallax speed
 
-  //Read here width and heigth of the window and update the heigth and width of the parallax-container and parallax-image.
+  var w_heigth = $( window ).height();
+  var w_width = $( window ).width();
+
+  $( ".parallax-container" ).height( w_width / 3 );
 }
---
+++ js/modMaterialize.js
@@ -0,0 +1,88 @@
+/*
+Image preloading
+*/
+$.fn.preload = function() {
+  this.each( function() {
+    $( '<img/>' )[ 0 ].src = this;
+  } );
+}
+// We can just create the array and call .preload() on it
+//$(['img1.jpg','img2.jpg','img3.jpg']).preload();
+
+
+
+
+
+
+/*
+Parallax with speed
+*/
+( function( $ ) {
+
+  $.fn.parallax = function( speed ) {
+
+    var parallaxSpeed = function( speed ) {
+      if ( isNaN( speed ) ) {
+        return 1;
+      } else {
+        return ( speed / 100 );
+      }
+    };
+
+    var window_width = $( window ).width();
+    // Parallax Scripts
+    return this.each( function( i ) {
+      var $this = $( this );
+      $this.addClass( 'parallax' );
+
+      function updateParallax( initial ) {
+
+        var container_height;
+        if ( window_width < 601 ) {
+          container_height = ( $this.height() > 0 ) ? $this.height() : $this.children( "img" ).height();
+        } else {
+          container_height = ( $this.height() > 0 ) ? $this.height() : 500;
+        }
+        var $img = $this.children( "img" ).first();
+        var img_height = $img.height();
+        var parallax_dist = img_height - container_height;
+        var bottom = $this.offset().top + container_height;
+        var top = $this.offset().top;
+        var scrollTop = $( window ).scrollTop();
+        var windowHeight = window.innerHeight;
+        var windowBottom = scrollTop + windowHeight;
+        var percentScrolled = ( windowBottom - top ) / ( container_height + windowHeight );
+        var parallax = Math.round( ( parallax_dist * percentScrolled ) * parallaxSpeed( speed ) );
+
+        if ( initial ) {
+          $img.css( 'display', 'block' );
+        }
+        if ( ( bottom > scrollTop ) && ( top < ( scrollTop + windowHeight ) ) ) {
+          $img.css( 'transform', "translate3D(-50%," + parallax + "px, 0)" );
+        }
+
+      }
+
+      // Wait for image load
+      $this.children( "img" ).one( "load", function() {
+        updateParallax( true );
+      } ).each( function() {
+        if ( this.complete ) $( this ).trigger( "load" );
+      } );
+
+      $( window ).scroll( function() {
+        window_width = $( window ).width();
+        updateParallax( false );
+      } );
+
+      $( window ).resize( function() {
+        window_width = $( window ).width();
+        updateParallax( false );
+      } );
+
+    } );
+
+  };
+}( jQuery ) );
+
+console.log( "JS Mods loaded" );
