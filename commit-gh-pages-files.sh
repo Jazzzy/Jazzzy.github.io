1957db4e63cd7ed6de5bf025e5629310f58510aa
Rubén Osorio López <ruben@osor.io>
2017-12-15 00:53:25 +0000

    Added note to commit batch file

---
+++ commit-gh-pages-files.sh
@@ -0,0 +1,6 @@
+
+# Following this guide to set up github pages
+# https://gohugo.io/hosting-and-deployment/hosting-on-github/
+
+hugo
+cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
